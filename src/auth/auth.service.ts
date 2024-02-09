586c23789a21e6fd7558ea7d158ad7bf711a8558
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-09 17:33:12 +0530

    add validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './auth.dto';
+
+const fakeUsers = [
+  {
+    id: 1,
+    userName: 'anson',
+    password: 'password',
+  },
+  {
+    id: 2,
+    userName: 'jack',
+    password: 'password',
+  },
+];
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser({ userName, password }: AuthPayloadDto) {
+    const findUser = fakeUsers.find((user) => user.userName === userName);
+    if (!findUser) return null;
+    if (password === findUser.password) {
+    }
+  }
+}
